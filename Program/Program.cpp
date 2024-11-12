5e8ba33866be5c21ce5b497f99a95e7e37db2d89
godkz1234 <ddddeong@naver.com>
2024-11-12 21:44:24 +0900

    (2024.11.12) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class SingleLinkedList 
+{
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+		int size;
+		Node * head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	};
+
+	void PushFront(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * newNode = new Node;
+
+			newNode->data = data;
+			newNode->next = head;
+
+			head = newNode;
+		}
+
+		size++;
+		
+	}
+
+};
+
+int main()
+{
+	SingleLinkedList<int> singleLinkedList;
+
+	singleLinkedList.PushFront(10);
+	singleLinkedList.PushFront(20); // [20]<------- [10]
+
+	
+	return 0;
+}
