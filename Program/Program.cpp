eca39a05a7050b3e53e470e1706f06bc6b01bb52
godkz1234 <ddddeong@naver.com>
2024-11-13 21:45:39 +0900

    (2024.11.13) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -44,7 +44,81 @@ public:
 		size++;
 		
 	}
+	void PoPFront()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+			
+			head = deleteNode->next;
 
+			delete deleteNode;
+			
+			size--;
+		}
+	}
+	void PushBack(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node* currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+
+			Node * newNode = new Node;
+
+			currentNode->next = newNode;
+
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+
+		size++;
+	}
+	void Show()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is empty" << std::endl;
+		}
+
+		Node* currentNode = head;
+
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << " ";
+			currentNode = currentNode->next;
+		}
+
+		cout << std::endl;
+	}
+	void PoPBack()
+	{
+		Node* deleteNode = head;
+
+		if (head->next == nullptr)
+		{
+			delete deleteNode;
+		}
+		else
+		{
+		
+		}
+	}
 };
 
 int main()
@@ -54,6 +128,17 @@ int main()
 	singleLinkedList.PushFront(10);
 	singleLinkedList.PushFront(20); // [20]<------- [10]
 
-	
+	// [20] - [10]
+
+	singleLinkedList.PushBack(5);
+	singleLinkedList.PushBack(0);
+
+	// [20] - [10] - [5] - [0]  
+
+	singleLinkedList.Show();
+
+	singleLinkedList.PoPFront();
+	singleLinkedList.PoPFront();
+
 	return 0;
 }
